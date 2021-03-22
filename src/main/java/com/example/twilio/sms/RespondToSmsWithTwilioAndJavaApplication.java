2e6dba7b70481375138c41975a0c7fc2eb513f9f
Matthew Gilliard <matthew@gilliard.lol>
2021-03-22 14:49:17 +0000

    Initial project as downloaded from start.spring.io

---
+++ src/main/java/com/example/twilio/sms/RespondToSmsWithTwilioAndJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.twilio.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespondToSmsWithTwilioAndJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespondToSmsWithTwilioAndJavaApplication.class, args);
+	}
+
+}
