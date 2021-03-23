bb7e53d02a58b183395ff50368282db4ee0c3ba0
Matthew Gilliard <matthew@gilliard.lol>
2021-03-23 13:34:27 +0000

    Adds dependency and code for returning TwiML to HTTP requests

--- src/main/java/com/example/twilio/sms/SmsWebhookHandler.java
+++ src/main/java/com/example/twilio/sms/SmsWebhookHandler.java
@@ -1,16 +1,35 @@
 package com.example.twilio.sms;
 
+import com.twilio.twiml.MessagingResponse;
+import com.twilio.twiml.messaging.Message;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 public class SmsWebhookHandler {
 
-    @PostMapping("/")
-    @ResponseBody
-    public String handleSmsWebhook(){
-        return "Hello from Twilio";
-    }
+    private final Map<String, Integer> messageCounts = new ConcurrentHashMap<>();
 
+    @PostMapping(value = "/", produces = "application/xml")
+    @ResponseBody
+    public String handleSmsWebhook(
+        @RequestParam("From") String from,
+        @RequestParam("Body") String body){
+
+        int thisMessageCount = messageCounts.compute(from, (k,v) -> (v == null) ? 1 : v+1);
+
+        String plural = (thisMessageCount > 1) ? "messages" : "message";
+        String message = String.format(
+            "☎️ Hello from Twilio. You've sent %d %s, and this one said '%s'",
+            thisMessageCount, plural, body);
+
+        return new MessagingResponse.Builder()
+            .message(new Message.Builder(message).build())
+            .build().toXml();
+    }
 }
